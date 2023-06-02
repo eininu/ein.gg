044896e308e7c9fb816bbec3b02b81446d22f0d8
ein <ein@jp9.org>
2023-06-02 13:31:43 +0200

    Wrap button into react component

---
+++ src/components/SpotifyMoodMapper/Auth.jsx
@@ -0,0 +1,13 @@
+const SpotifyAuth = () => {
+    return (
+        <button type="button"
+                   className="text-[#191414] bg-[#1DB954] hover:bg-[#FFFFFF] hover:border-2 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#050708]/50 dark:hover:bg-[#FFFFFF] mr-2 mb-2 mt-2">
+        <img className="w-5 h-5 mr-2 -ml-1"
+             src="https://www.freepnglogos.com/uploads/spotify-logo-png/spotify-icon-black-17.png" alt=""
+             data-encore-id="image"/>
+            Sign in with Spotify
+        </button>
+    )
+}
+
+export default SpotifyAuth;
