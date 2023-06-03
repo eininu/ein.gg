bcb9230ad810e589706327b48924f4a4c2158b57
ein <ein@jp9.org>
2023-06-04 01:53:17 +0200

    Testing cloudflare middleware to cloud functions

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,13 +1,15 @@
-export async function onRequestGet({ request }) {
-  const url = new URL(request.url);
+async function proxyToGCF(context) {
+  const url = new URL(context.request.url);
 
   if (url.pathname.startsWith("/api/")) {
     const gcfUrl =
       "https://us-central1-ein-gg.cloudfunctions.net/helloWorld" +
       url.pathname.slice(4);
-    const newRequest = new Request(gcfUrl, request);
+    const newRequest = new Request(gcfUrl, context.request);
     return await fetch(newRequest);
   }
 
-  return null;
+  return await context.next();
 }
+
+export const onRequest = [proxyToGCF];
