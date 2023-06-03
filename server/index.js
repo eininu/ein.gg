ad5e72590ed8c875822d8817e6c1f421a547b541
ein <ein@jp9.org>
2023-06-04 00:50:25 +0200

    Create custom route with dynamic data

--- server/index.js
+++ server/index.js
@@ -10,4 +10,14 @@ app.get("/otherpath", (req, res) => {
   res.status(200).send("You're on the other path!");
 });
 
+app.get("/sum/:a/:b", (req, res) => {
+  const a = Number(req.params.a);
+  const b = Number(req.params.b);
+  const sum = (a, b) => {
+    return a + b;
+  };
+  const c = sum(a, b);
+  res.status(200).send({ a, b, c });
+});
+
 exports.helloWorld = app;
