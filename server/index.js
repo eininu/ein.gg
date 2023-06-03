ad4589ebbc23f57d7f7a6f62dff03d5be143cf68
ein <ein@jp9.org>
2023-06-04 00:41:54 +0200

    Setup express.js for test a few routes

--- server/index.js
+++ server/index.js
@@ -1,10 +1,13 @@
-/**
- * Responds to any HTTP request.
- *
- * @param {!express:Request} req HTTP request context.
- * @param {!express:Response} res HTTP response context.
- */
-exports.helloWorld = (req, res) => {
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
   let message = req.query.message || req.body.message || "Hello World!";
   res.status(200).send(message);
-};
+});
+
+app.get("/otherpath", (req, res) => {
+  res.status(200).send("You're on the other path!");
+});
+
+exports.helloWorld = app;
