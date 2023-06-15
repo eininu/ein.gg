004077dc9815b8240a239223cfa5ab80227677c0
ein <ein@jp9.org>
2023-06-15 06:24:47 +0200

    Add todo

--- src/components/Oowg/Oowg.jsx
+++ src/components/Oowg/Oowg.jsx
@@ -1,3 +1,5 @@
+// TODO: Add custom code focus element
+
 // TODO: Add favicon.ico
 // TODO: Stop adding pictures if table has paragraphs
 // TODO: Remove demo reloading after switch the lang (just load language once by browser language)
