59b9f5a6ff7eea09685135b8d7aa9ef1582ad022
ein <ein@jp9.org>
2023-06-15 18:10:27 +0200

    Remove dark gray background for demo area

--- src/components/Oowg/Oowg.jsx
+++ src/components/Oowg/Oowg.jsx
@@ -1306,7 +1306,7 @@ export default function Oowg(props) {
 
               {/* Side content */}
               <div
-                className={`lg:block order-first lg:order-last lg:col-span-4 p-5 lg:p-6 bg-white shadow-sm rounded-lg dark:bg-gray-900`}
+                className={`lg:block order-first lg:order-last lg:col-span-4 p-5 lg:p-6 bg-white shadow-sm rounded-lg`}
               >
                 {/*flex items-center justify-center rounded-xl bg-gray-50 border-2 border-dashed border-gray-200 text-gray-400 py-64 dark:bg-gray-800 dark:border-gray-700*/}
                 {/*<div className="">*/}
