5563d805a2a50dc1c4757523007740013b05ff19
ein <ein@jp9.org>
2023-06-15 15:31:02 +0200

    Fix show demo table in zip

--- src/components/Oowg/Oowg.jsx
+++ src/components/Oowg/Oowg.jsx
@@ -193,6 +193,7 @@ export default function Oowg(props) {
         ratingTableHead,
         ratingTableBody,
         customFocusElementCode,
+        showDemoTable,
       })
     );
     if (amp) {
